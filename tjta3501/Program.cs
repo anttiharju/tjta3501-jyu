01ff4ecc55056ca6a95e70038ef20ed9379f69ab
Antti Harju <antti@harju.io>
2021-05-15 21:22:48 +0300

    Luo vs2019 projekti

---
+++ tjta3501/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace tjta3501
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
